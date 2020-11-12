d70c595a3cf63c57ac13321d8948c8afde083955
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-12 14:16:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
+    if  v < lowest_value || lowest_value == nil
       lowest_value = v
       lowest_key = k
     end
